af61ce7da82fc59d70ce13f9c17428258a1cc7e2
liulihua <hc_liulihua@163.com>
2020-05-22 13:57:34 +0800

    修改

--- main.js
+++ main.js
@@ -1,33 +1,230 @@
-const { app, BrowserWindow } = require('electron')
+﻿const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const path = require('path')
+if (process.mas) app.setName("科尔实验室通风系统智能控制软件")
+let mainWindow
 
-createWindow = () => {
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+function createWindow(callback) {
+  let windowOptions = {
+    width: 1400,
+    height: 900,
+    frame: true, //frame: false 参数用来设置无边框；
+    title: app.name
+  }
+
+  if (process.platform === 'linux') {
+    windowOptions.icon = path.join(__dirname, '/app/ico/your-ico.png')
+  }
+
+  mainWindow = new BrowserWindow(windowOptions);
+
+  mainWindow.loadFile(path.join(__dirname, '/src/index.html'))
+  // mainWindow.loadURL('https://www.gm5580.com/gmlzg/') // 方法三
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  });
+
+}
+app.on('ready', function () {
+  // 设置菜单部分
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu) // 设置菜单部分
+  createWindow()
+  // 在主进程中.-下载事件
+  mainWindow.webContents.session.on('will-download', (event, item, webContents) => {
+    // 设置保存路径,使Electron不提示保存对话框。
+    //item.setSavePath('/tmp/save.pdf')
+    item.on('updated', (event, state) => {
+      if (state === 'interrupted') {
+        console.log('Download is interrupted but can be resumed')
+      } else if (state === 'progressing') {
+        if (item.isPaused()) {
+          console.log('Download is paused')
+        } else {
+          console.log(`Received bytes: ${item.getReceivedBytes()}`)
+        }
+      }
+    })
+    item.once('done', (event, state) => {
+      if (state === 'completed') {
+        console.log('Download successfully')
+      } else {
+        console.log(`Download failed: ${state}`)
+      }
+    })
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
+app.on('browser-window-created', function () {
+  let reopenMenuItem = findReopenMenuItem()
+  if (reopenMenuItem) reopenMenuItem.enabled = false
+})
+
+app.on('window-all-closed', function () {
+  let reopenMenuItem = findReopenMenuItem()
+  if (reopenMenuItem) reopenMenuItem.enabled = true
+  app.quit()
+})
+
+
+/**
+ * 注册键盘快捷键
+ * 其中：label: '切换开发者工具',这个可以在发布时注释掉
+ */
+let template = [
+  {
+    label: '窗口',
+    role: 'window',
+    submenu: [{
+      label: '最小化',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    }, {
+      label: '关闭',
+      accelerator: 'CmdOrCtrl+W',
+      role: 'close'
+    }, {
+      label: '切换开发者工具',
+      accelerator: (function () {
+        if (process.platform === 'darwin') {
+          return 'Alt+Command+I'
+        } else {
+          return 'Ctrl+Shift+I'
+        }
+      })(),
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.toggleDevTools()
+        }
+      }
+    }
+      , {
+      type: 'separator'
+    }]
+  },
+  {
+    label: '操作',
+    submenu: [{
+      label: '复制',
+      accelerator: 'CmdOrCtrl+C',
+      role: 'copy'
+    }, {
+      label: '粘贴',
+      accelerator: 'CmdOrCtrl+V',
+      role: 'paste'
+    }, {
+      label: '重新加载',
+      accelerator: 'CmdOrCtrl+R',
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          // on reload, start fresh and close any old
+          // open secondary windows
+          if (focusedWindow.id === 1) {
+            BrowserWindow.getAllWindows().forEach(function (win) {
+              if (win.id > 1) {
+                win.close()
+              }
+            })
+          }
+          focusedWindow.reload()
+        }
+      }
+    }]
+  }
+]
+
+/**
+ * 增加更新相关的菜单选项
+ */
+/**function addUpdateMenuItems(items, position) {
+  if (process.mas) return
+
+  const version = electron.app.getVersion()
+  let updateItems = [{
+    label: `Version ${version}`,
+    enabled: false
+  }, {
+    label: 'Checking for Update',
+    enabled: false,
+    key: 'checkingForUpdate'
+  }, {
+    label: 'Check for Update',
+    visible: false,
+    key: 'checkForUpdate',
+    click: function () {
+      require('electron').autoUpdater.checkForUpdates()
+    }
+  }, {
+    label: 'Restart and Install Update',
+    enabled: true,
+    visible: false,
+    key: 'restartToUpdate',
+    click: function () {
+      require('electron').autoUpdater.quitAndInstall()
+    }
+  }]
+
+  items.splice.apply(items, [position, 0].concat(updateItems))
+}*/
+
+function findReopenMenuItem() {
+  const menu = Menu.getApplicationMenu()
+  if (!menu) return
+
+  let reopenMenuItem
+  menu.items.forEach(function (item) {
+    if (item.submenu) {
+      item.submenu.items.forEach(function (item) {
+        if (item.key === 'reopenMenuItem') {
+          reopenMenuItem = item
+        }
+      })
     }
   })
-  // 为应用加载index.html
-  win.loadFile('./src/login.html')
-  // 打开开发者工具
-  win.webContents.openDevTools()
+  return reopenMenuItem
 }
 
-app.whenReady().then(createWindow)
+// 针对Mac端的一些配置
+if (process.platform === 'darwin') {
+  const name = electron.app.getName()
+  template.unshift({
+    label: name,
+    submenu: [{
+      label: 'Quit ( 退出 )',
+      accelerator: 'Command+Q',
+      click: function () {
+        app.quit()
+      }
+    }]
+  })
 
-app.on('window-all-closed', () => {
-  // 在masOS上，除非用户Cmd+Q 确定地的退出
-  // 否者绝大部分应用及其菜单栏保持激活
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  // Window menu.
+  template[3].submenu.push({
+    type: 'separator'
+  }, {
+    label: 'Bring All to Front',
+    role: 'front'
+  })
 
-app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+  //addUpdateMenuItems(template[0].submenu, 1)
+}
+
+// 针对Windows端的一些配置
+/*if (process.platform === 'win32') {
+  const helpMenu = template[template.length - 1].submenu
+  addUpdateMenuItems(helpMenu, 0)
+}*/
+
+
+
